6699f436db8a707efb813314c7565c72491d316f
Kelly <kellybytes.ca@gmail.com>
2025-12-28 13:39:00 -0700

    Initial commit: create a server

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a tutorial for intro to backend
---
+++ backend/src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`
+    );
+    console.log(`\n MongoDB connected! 
+      ${connectionInstance.connection.host}`);
+  } catch (err) {
+    console.log('MongoDB connection failed', err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
